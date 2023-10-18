867cfad2be71c1e502721f376b522adc21dd191d
devmasud1 <devmahmud.masu1@gmail.com>
2023-10-18 17:25:31 +0600

    post single product & get all products  method

--- index.js
+++ index.js
@@ -10,7 +10,6 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.aunb3y8.mongodb.net/?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, {
@@ -24,6 +23,19 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     await client.connect();
+
+    const productsCollection = client.db("ecoTechDB").collection("products");
+
+    app.get("/products", async (req, res) => {
+      const result = await productsCollection.find().toArray();
+      res.send(result);
+    });
+
+    app.post("/product", async (req, res) => {
+      const newProduct = req.body;
+      const result = await productsCollection.insertOne(newProduct);
+      res.send(result);
+    });
 
     await client.db("admin").command({ ping: 1 });
     console.log("connected to MongoDB!");
